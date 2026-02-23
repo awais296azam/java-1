a4c338415914b49d691c47597bc91f001c7637fb
Awais Azam <168289709+awais296azam@users.noreply.github.com>
2026-02-23 16:27:43 +0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,133 @@
+// // console.log ("This is my First project");
+// let myname,age,salary;
+// age= "Twentyfour";
+// myname="Awais Azam";
+// salary="40000";
+// // let months=age * 12;
+// // days=months * 30;
+// console.log("Name :", myname, "Age:" , age,"Salary:",salary);
+
+// // console.log ("Age in Months", months, "months");
+// // console.log("Age in Days",days,"days");
+
+// // let A=20;
+// // let B=5;
+// // console.log("value a",A);
+// // console.log("value b", B);
+// // console.log("value a+b", A+B);
+// // console.log("value a*b",A*B);
+// // console.log("value a/b",A/B);
+
+// let salary="5000";
+// let myname="Awais Azam";
+// let muslim="true";
+// let age ;
+// let chk= null;
+// console.log("Naame :", myname);
+// console.log("variable type :", typeof myname);
+// console.log( "salary :", 5000);
+// console.log( "variable type:", typeof 5000);
+// console.log("muslim :", muslim);
+// console.log("Variable type :", typeof muslim);
+// console.log("age :", age)
+// console.log (chk);
+
+// let person={
+//      firstName : "Muhammad",
+//      lastName : "Awais Azam",
+//      age : 24,
+//      salarry : 50000,
+//      perm : true,
+// };
+// // console.log  (person);
+// // console.log (typeof person);
+// console.log ( "Salarry of a person" , person["salarry"]); 
+// console.log ( "Age of a person" , person["age"]); 
+
+// let a=5;
+// // a++;
+// console.log ("Value of a:", a++);
+// console.log ("Value of a:", a);
+
+// let b=3;
+// console.log ("Value of a variable", a, "Value of b variable", b);
+// console.log ("a+b=",a+b);
+// console.log ("a-b=",a-b);
+// console.log ("a*b=",a*b);
+// console.log ("a/b=",a/b);
+// console.log ("a%b=",a%b);
+// console.log ("a**b=",a**b);
+// let a=5;
+// a=a+1;
+// a +=1;
+// console.log("Value of a+=2:", a+=2);
+// console.log("Value of a-=2:", a-=2);
+// console.log("Value of a*=2:", a*=2);
+// console.log("Value of a%=2:", a%=2);
+// console.log("Value of a**=2:", a**=2);
+// let a=20;
+// let b=50;
+// console.log("if a==b", a==b);
+// let a=20;
+// let b="50";
+// console.log("if a===b", a===b);
+// let a=50;
+// let b=20;
+// console.log("if a!=b", a!=b);
+// let a=50;
+// let b=50;
+// console.log("if a!=b", a!=b);
+// let a=50;
+// let b="50";
+// console.log("if a!==b", a!==b);
+// let a=50;
+// let b=60;
+// console.log("if 50<60", a<b);
+// let a=50;
+// let b=50;
+// console.log("if 50<60", a<b);
+//  let age=20;
+//  let nat= "pakistan";
+//  console.log (" First EXP = age > 18", age > 18);
+//  console.log("First EXP = Pakistani", nat=="pakistan");
+//  console.log (" Final Dicission = issue CNIC", age >18 && nat=="pakistan");
+//  let age=20;
+//  let nat= "uk";
+//  console.log (" First EXP = age > 18", age > 18);
+//  console.log("First EXP = Pakistani", nat=="pakistan");
+//  console.log (" Final Dicission = issue CNIC", age >18 && nat=="pakistan");
+//  let age=65;
+//  let govtemp= true;
+//  console.log (" First EXP = age > 60 :", age > 60);
+//  console.log(" secound EXP = govt Emp : ", govtemp==true);
+//  console.log (" Final Dicission = Free treatment : ", age >60 && govtemp==true);
+// let marks, sstatus
+// marks = 50;
+// marks>=33?  sstatus= "Pass": sstatus="Fail";
+// console.log ("student ststus", sstatus);
+// let marks, sstatus
+// marks = 30;
+// marks>=33?  sstatus= "Pass": sstatus="Fail";
+// console.log ("student ststus", sstatus);
+// let marks, sstatus
+// marks = 33;
+// marks>=33?  sstatus= "Pass": sstatus="Fail";
+// console.log ("student ststus", sstatus);
+// let age=15;
+// age>=18? console.log ("he can drive"):console.log ("He cannot drive");
+// let marks=25;
+// if(marks>33)
+//      console.log ("Student is pass");
+// else 
+//      console.log("Student is fail");
+// console.log ("univeristy of agriculture Fasilabad");
+// let marks=55;
+// if(marks>33)
+// {
+//      console.log ("Student is pass");
+//      }
+// else 
+// {
+//      console.log("Student is fail");
+// console.log ("univeristy of agriculture system");
+// }
